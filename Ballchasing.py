18f1b681cee87dfc22f734bf7ca1cb16279e07b0
Xylot <Joseph.Meriam@gmail.com>
2020-04-24 16:29:45 -0500

    Converted variables and functions to snake case

--- Ballchasing.py
+++ Ballchasing.py
@@ -15,30 +15,30 @@ def main():
     args = parse_args()
     uploader = Uploader(args.folderpath, args.visibility)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(uploader.asyncUpload())
-    uploader.logResponses(uploader.asyncPayloads)  
+    loop.run_until_complete(uploader.upload_async())
+    uploader.log_responses(uploader.asyncPayloads)  
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--folderpath', type=str, default=getUserDemoPath(), help='Path of the folder to upload')
+    parser.add_argument('-p', '--folderpath', type=str, default=get_user_demo_path(), help='Path of the folder to upload')
     parser.add_argument('-v', '--visibility', type=str, default='public', choices=['public', 'private', 'unlisted'], help='Visibility of the replays')
     return parser.parse_args()
 
 # Finds the path Rocket League uses to store the replay files on the system
 # Default windows path: C:/Users/%USERNAME%/Documents/My Games/Rocket League/TAGame/Demos/
 # Default macos path: '~/Library/Application Support/Rocket League/TAGame/Demos'
-def getUserDemoPath():
+def get_user_demo_path():
     if platform.system() == 'Windows':
         CSIDL_PERSONAL= 5
         SHGFP_TYPE_CURRENT= 0
         buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
         ctypes.windll.shell32.SHGetFolderPathW(0, CSIDL_PERSONAL, 0, SHGFP_TYPE_CURRENT, buf)
-        dPath = str(buf.value) + '\\My Games\\Rocket League\\TAGame\\Demos\\'
-        return dPath
+        demo_path = str(buf.value) + '\\My Games\\Rocket League\\TAGame\\Demos\\'
+        return demo_path
     elif platform.system() == 'Darwin':
-        dPath = '/Library/Application Support/Rocket League/TAGame/Demos/'
-        return dPath
+        demo_path = '/Library/Application Support/Rocket League/TAGame/Demos/'
+        return demo_path
 
 
 if __name__ == '__main__':
--- Uploader.py
+++ Uploader.py
@@ -16,81 +16,82 @@ except:
 
 class Uploader:
 
-    def __init__(self, demoPath, visibility):
-        self.demoPath = demoPath
+    def __init__(self, demo_path, visibility):
+        self.demo_path = demo_path
         self.visibility = visibility
-        self.syncPayloads = {}
-        self.asyncPayloads = []
-        self.getUploadParameters()
-        self.fileList = self.getFileList()
-        self.replayList = self.filterFileList(self.fileList)
-        self.replayCount = len(self.replayList)
-        self.openAll()
+        self.sync_payloads = {}
+        self.async_payloads = []
+        self.get_upload_parameters()
+        self.file_list = self.get_file_list()
+        self.replay_list = self.filter_file_list(self.file_list)
+        self.replay_count = len(self.replay_list)
+        self.open_all()
 
-    def getUploadParameters(self):
-        self.uploadURL = 'https://ballchasing.com/api/v2/upload?visibility=' + self.visibility
-        self.authFilepath = 'Resources/auth'
-        self.authID = self.getAuthID()
-        self.authParameter = {'Authorization': self.authID}
-        self.tempResponsePath = 'Resources/Logs/tempResponse'
-        self.responseLogPath = 'Resources/Logs/Responses'
+    def get_upload_parameters(self):
+        cwd = os.path.dirname(__file__)
+        self.upload_url = 'https://ballchasing.com/api/v2/upload?visibility=' + self.visibility
+        self.auth_file_path = cwd + '/Resources/auth'
+        self.auth_id = self.get_auth_id()
+        self.auth_parameter = {'Authorization': self.auth_id}
+        self.temp_response_path = cwd + '/Resources/Logs/tempResponse'
+        self.response_log_path = cwd + '/Resources/Logs/Responses'
 
-    def getAuthID(self):
-        with open(self.authFilepath, 'r') as file:
-            authID = file.read()
-        return authID
+    def get_auth_id(self):
+        with open(self.auth_file_path, 'r') as file:
+            auth_id = file.read()
+        return auth_id
 
-    def getFileList(self, demoPath=None):
-        if demoPath is None:
-            demoPath = self.demoPath
-        return [(x[0], time.ctime(x[1].st_ctime), os.path.getsize(x[0])) for x in sorted([((demoPath + fn), os.stat((demoPath + fn))) for fn in os.listdir(demoPath)], key = lambda x: x[1].st_ctime)]
+    def get_file_list(self, demo_path=None):
+        if demo_path is None:
+            demo_path = self.demo_path
+        return [(x[0], time.ctime(x[1].st_ctime), os.path.getsize(x[0])) for x in sorted([((demo_path + fn), os.stat((demo_path + fn))) for fn in os.listdir(demo_path)], key = lambda x: x[1].st_ctime)]
 
-    def filterFileList(self, fileList):
-        filteredList = []
-        for replay in fileList:
+    def filter_file_list(self, file_list):
+        filtered_list = []
+        for replay in file_list:
             if replay[2] > 5000 and ('.replay' in replay[0]):
-                filteredList.append(replay)
-        return filteredList
+                filtered_list.append(replay)
+        return filtered_list
 
-    def openAll(self):
-        self.openedReplays = []
-        for replay in self.replayList:
-            self.openedReplays.append({'data': {'file': open(replay[0], 'rb')}, 'Name':os.path.basename(replay[0])})
+    def open_all(self):
+        self.opened_replays = []
+        for replay in self.replay_list:
+            self.opened_replays.append({'data': {'file': open(replay[0], 'rb')}, 'Name':os.path.basename(replay[0])})
 
-    def getTempResponse(self):
-        if os.path.exists(self.tempResponsePath):
-            return self.readJSON()
+    def get_temp_response(self):
+        if os.path.exists(self.temp_response_path):
+            return self.read_json()
         return None
 
-    def readJSON(self):
-        with open(self.tempResponsePath, 'r') as file:
+    def read_json(self):
+        with open(self.temp_response_path, 'r') as file:
             response = json.load(file)
         return response
 
-    def writeJSON(self, data):
-        logPath = self.responseLogPath + str(datetime.datetime.now().strftime(" %Y-%m-%d %H-%M-%S"))
+    def write_json(self, data):
+        logPath = self.response_log_path + str(datetime.datetime.now().strftime(" %Y-%m-%d %H-%M-%S"))
         with open(logPath, 'w') as file:
             json.dump(data, file, indent=4)
 
-    def logResponses(self, data=None):
+    def log_responses(self, data=None):
         if data is None:
-            data = self.asyncPayloads
-        self.writeJSON(data)
+            data = self.async_payloads
+        self.write_json(data)
 
-    def syncUpload(self):
-        self.responseList['UploadInformation'] = {'Path':self.demoPath, 'Visibility':self.visibility, 'ReplayCount':self.replayCount}
-        for replay in tqdm(self.replayList, total=self.replayCount):
-            subprocess.call(['curl', '-s', '-o', self.tempResponsePath, '-F', 'file=@' + replay[0],'-H', 'Authorization:' + self.authID, self.uploadURL], shell=False)
-            tempResponse = self.getTempResponse()
-            self.responseList[replay[0]] = tempResponse
-        self.logResponses()
+    def upload_sync(self):
+        self.response_list['UploadInformation'] = {'Path':self.demo_path, 'Visibility':self.visibility, 'ReplayCount':self.replay_count}
+        for replay in tqdm(self.replay_list, total=self.replay_count):
+            subprocess.call(['curl', '-s', '-o', self.temp_response_path, '-F', 'file=@' + replay[0],'-H', 'Authorization:' + self.auth_id, self.upload_url], shell=False)
+            temp_response = self.get_temp_response()
+            self.response_list[replay[0]] = temp_response
+        self.log_responses()
 
-    async def asyncUpload(self):
+    async def upload_async(self):
         async with aiohttp.ClientSession() as self.session:
-            responses = [await future for future in tqdm(asyncio.as_completed(map(self.asyncPost, self.openedReplays)), total=self.replayCount)]
+            responses = [await future for future in tqdm(asyncio.as_completed(map(self.post_async, self.opened_replays)), total=self.replay_count)]
 
-    async def asyncPost(self, replay):
-        async with self.session.post(self.uploadURL, headers=self.authParameter, data=replay['data']) as response:
-            self.asyncPayloads.append([replay['Name'], await response.json()])
+    async def post_async(self, replay):
+        async with self.session.post(self.upload_url, headers=self.auth_parameter, data=replay['data']) as response:
+            self.async_payloads.append([replay['Name'], await response.json()])
 
 
